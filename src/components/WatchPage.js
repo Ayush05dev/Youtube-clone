a1383d97c95a5d1a187c9fe623d3387db1d06033
Ayush-8955 <ayush058955@gmail.com>
2024-12-29 21:02:01 +0530

    some changes like adding env file

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect,useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { closeMenu } from '../utils/appSlice'
 import { useSearchParams,Link } from 'react-router-dom'
 import { formatCompactNumber } from '../utils/helper'
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,13 @@
-const GOOGLE_API_KEY="AIzaSyDoX7pZYnllgYcKtFrVIPA79HticsvMeFQ"
+const REACT_APP_GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
-export const YOUTUBE_VIDEOS_API ="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
+export const YOUTUBE_VIDEOS_API ="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+REACT_APP_GOOGLE_API_KEY;
 
-export const YOUTUBE_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=' + GOOGLE_API_KEY +'&id='; 
+export const YOUTUBE_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=' + REACT_APP_GOOGLE_API_KEY +'&id='; 
 
 export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 
-export const YOUTUBE_RECOMENDED_VIDEO_LIST_API = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=50&key=${GOOGLE_API_KEY}&relatedToVideoId=`;
+export const YOUTUBE_RECOMENDED_VIDEO_LIST_API = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=50&key=${REACT_APP_GOOGLE_API_KEY}&relatedToVideoId=`;
 
-export const YOUTUBE_SEARCH_VIDEO_WITH_QUERY_API = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=${GOOGLE_API_KEY}&q=`;
+export const YOUTUBE_SEARCH_VIDEO_WITH_QUERY_API = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=${REACT_APP_GOOGLE_API_KEY}&q=`;
 
-export const CATEGORIES_API = "https://youtube.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=IN&key=" + GOOGLE_API_KEY;
+export const CATEGORIES_API = "https://youtube.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=IN&key=" + REACT_APP_GOOGLE_API_KEY;
